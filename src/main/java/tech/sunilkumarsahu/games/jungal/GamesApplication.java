d4286e9743a6cbc8f6cfe8cc0bc904c7d23d4a61
Sunil Kumar Sahu <sunilkumarsahu@google.com>
2019-01-10 17:38:39 +0530

    Spring project init

---
+++ src/main/java/tech/sunilkumarsahu/games/jungal/GamesApplication.java
@@ -0,0 +1,14 @@
+package tech.sunilkumarsahu.games.jungal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesApplication.class, args);
+	}
+
+}
+
