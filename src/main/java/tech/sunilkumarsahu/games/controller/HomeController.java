e401e65cb972ca70f0cf433ef37bb4caa2adb89c
Sunil Kumar Sahu <sunilkumarsahu@google.com>
2019-01-10 18:20:57 +0530

    Some work

---
+++ src/main/java/tech/sunilkumarsahu/games/controller/HomeController.java
@@ -0,0 +1,14 @@
+package tech.sunilkumarsahu.games.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/index")
+	public String home() {
+		return "index.html";
+	}
+
+}
